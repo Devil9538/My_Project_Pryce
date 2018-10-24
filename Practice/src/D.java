618d974e256dd2ee91b69b6303d84328395212a2
Sachin_Mishra_Pryce <sachin.mishra0112@gmail.com>
2018-10-24 18:00:19 +0530

    My First Commit

---
+++ Practice/src/D.java
@@ -0,0 +1,14 @@
+
+public class D {
+	
+	
+		public static void main(String [] args) {
+			int i;
+			int j;
+			for(i=0,j=10; i<5; i++, j--) {
+				System.out.println(i+" "+j);
+				
+			}
+		}
+
+}
